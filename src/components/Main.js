e77c4213a7c87b89c5cbb9d6dea85afdb225e7e6
Greg Altman <greg@flocabulary.com>
2016-10-03 04:28:14 -0400

    lightly functional madlibs game

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,76 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
 
-import React from 'react';
+var React = require('react');
 
-let yeomanImage = require('../images/yeoman.png');
-
-class AppComponent extends React.Component {
-  render() {
+var AppComponent = React.createClass({
+  getInitialState: function() {
+    var inputs = {};
+    var regexp = /%&(.*?)&%/gi;
+    var result;
+    while (result = regexp.exec(this.props.text)) {
+      inputs[result[1]] = '';
+    }
+    return {
+      inputs: inputs,
+      submitted: false
+    };
+  },
+  getDefaultProps: function() {
+    return {
+      text: "Number One\nFreedom of religion, speech, and press,\nPlus you can assemble in crowds and %&verb&%.\n\nNumber Two\nRight to bear arms and cannons,\nI bet the Minutemen didn't know about handguns.\n\nNumber Three\nWhen soldiers gets sleepy,\nYou don't have to let them sleep up on your %&noun&%.\n\nNumber Four\nNo one can search and seize. It protects me, \nUnless people have a warrant to arrest me.\n\nNumber Five\nIf you arrest me, respect me.\nSorry, Alex, there's no Double Jeopardy.\nWhat'd you do after school?\n\"I plead the fifth.\"\nWhat'd you do after that, dude?\n\"I plead the fifth.\"\nI don't have to incriminate myself or risk my health,\nWhenver I'm in trouble, I just plead the fifth.\n\nNumber Six\nYou must process me %&word ending in -ly&%.\n\nNumber Seven\nIn front of my peers on the jury.\n\nNumber Eight\nYou can't use cruel or unusual punishment.\nYou can't make me drink %&liquid&% for the fun of it.\n\nNumber Nine\nThe people get more than these rights.\n\nNumber Ten\nStates can make other laws, and they just might.\n\nThis is the Bill of Rights."
+    };
+  },
+  onSubmit: function(event) {
+    event.preventDefault()
+    this.setState({
+      submittedText: this.props.text.replace(
+        /%&(.*?)&%/gi,
+        (match, p1) => `<span class="user-inputted">${this.state.inputs[p1]}</span>`
+      ).replace(
+        /\n/gi,
+        '<br />'
+      )
+    });
+  },
+  render: function() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        {
+          this.state.submittedText
+          ? (
+              <div dangerouslySetInnerHTML={{__html: this.state.submittedText}} />
+            )
+          : (
+              <form onSubmit={this.onSubmit}>
+                {
+                  Object.keys(this.state.inputs).map(
+                    inputName => (
+                      <div>
+                        <label>{inputName}</label>
+                        <input
+                          type='text'
+                          key={inputName}
+                          name={inputName}
+                          value={this.state.inputs[inputName]}
+                          onChange={
+                            e => {
+                              this.state.inputs[inputName] = e.target.value;
+                              this.setState({inputs: this.state.inputs});
+                            }
+                          }
+                        />
+                      </div>
+                    )
+                  )
+                }
+                <button type="submit">interpolate!</button>
+              </form>
+            )
+        }
       </div>
     );
   }
-}
+})
 
-AppComponent.defaultProps = {
-};
-
-export default AppComponent;
+module.exports = AppComponent;
